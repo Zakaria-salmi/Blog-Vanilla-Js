82376f3d3da3f47e8974efcde04a6642bafa66ef
Zakaria Salmi <zakaria.salmi140302@gmail.com>
2024-08-12 21:51:02 +0200

    Creating article page

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "src/index.html"),
-                form: resolve(__dirname, "./src/form/form.html"),
+                form: resolve(__dirname, "src/form/form.html"),
             },
         },
     },
