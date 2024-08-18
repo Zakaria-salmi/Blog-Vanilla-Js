3a04e228d3b9f16d1bdbd1c82dbffbdfc5843487
Zakaria Salmi <zakaria.salmi140302@gmail.com>
2024-08-18 20:16:16 +0200

    add responsive to the blog

--- src/main.js
+++ src/main.js
@@ -4,30 +4,73 @@ import "./index.scss";
 const articleContainerElement = document.querySelector(".article-container");
 
 const createArticles = (articles) => {
-    const arcticlesDOM = articles.map((article) => {
+    if (!Array.isArray(articles)) {
+        articles = [articles];
+    }
+
+    const articlesDOM = articles.map((article) => {
         const articleDOM = document.createElement("div");
         articleDOM.classList.add("article");
-        articleDOM.innerHTML = `
-            <img
-                src="${article.img}"
-                alt="profile"
-            />
-            <h2>${article.title}</h2>
-            <p class="article-author">${article.author} - ${article.category}</p>
-            <p class="article-content">
-                ${article.content}
-            </p>
-            <div class="article-actions">
-                <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
-                <button class="btn btn-primary">Modifier</button>
-            </div>
+
+        const imgElement = document.createElement("img");
+        imgElement.src = article.img;
+        imgElement.alt = "profile";
+
+        const titleElement = document.createElement("h2");
+        titleElement.textContent = article.title;
+
+        const authorElement = document.createElement("p");
+        authorElement.classList.add("article-author");
+        authorElement.textContent = `${article.author} - ${article.category}`;
+
+        const contentElement = document.createElement("p");
+        contentElement.classList.add("article-content");
+        contentElement.textContent = article.content;
+
+        const actionsElement = document.createElement("div");
+        actionsElement.classList.add("article-actions");
+        actionsElement.innerHTML = `
+            <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
+            <button class="btn btn-primary">Modifier</button>
         `;
-        console.log(article);
+
+        articleDOM.append(
+            imgElement,
+            titleElement,
+            authorElement,
+            contentElement,
+            actionsElement
+        );
+
         return articleDOM;
     });
 
     articleContainerElement.innerHTML = "";
-    articleContainerElement.append(...arcticlesDOM);
+    articleContainerElement.append(...articlesDOM);
+
+    const deleteButtons =
+        articleContainerElement.querySelectorAll(".btn-danger");
+
+    deleteButtons.forEach((button) => {
+        button.addEventListener("click", async (event) => {
+            try {
+                const target = event.target;
+                const articleId = target.dataset.id;
+
+                const response = await fetch(
+                    `https://restapi.fr/api/articles/${articleId}`,
+                    {
+                        method: "DELETE",
+                    }
+                );
+                const body = await response.json();
+                console.log(body);
+                fetchArticle();
+            } catch (e) {
+                console.error(e);
+            }
+        });
+    });
 };
 
 const fetchArticle = async () => {
